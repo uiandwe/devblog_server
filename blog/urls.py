22962c5714e04fb71fbb322d6eb5d1455fcbc009
uiandwe <uiandwe@gmail.com>
2016-10-07 19:38:28 +0900

    add blog init page

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+__author__ = 'uiandwe'
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(regex=r"^$", view=views.BlogListView.as_view(), name="list"),
+    # url(regex=r"^(?P<pk>\d+)/$", view=views.PostDetailView.as_view(), name="detail"),
+    # url(regex=r"^(?P<pk>\d+)/results/$", view=views.PostResultsView.as_view(), name="results"),
+    # url(regex=r"^(?P<pk>\d+)/update/$", view=views.PostUpdateView.as_view(), name="update"),
+    #
+    # url(r"^(?P<pk>\d+)/detail/$", 'post.views.post_detail'),
+    # url(regex=r"^(?P<pk>\d+)/mixin/$", view=views.PostMixinListView.as_view(), name="mixin"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView, TemplateView
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+
+# from .models import Post
+# from .forms import PostForm
+# from .decorators import check_draft
+
+
+class PostListView(ListView):
+    # model = Post
+    template_name = "blog/blog_list.html"
