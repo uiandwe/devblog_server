7ee7bcd91da2930e62fea30dfbb151eeca63f49b
uiandwe <uiandwe@gmail.com>
2017-01-17 00:51:42 +0900

    vue for 적용

---
+++ static/vuejs/app.js
@@ -0,0 +1,39 @@
+
+
+Vue.config.delimiters = ["[[","]]"]
+
+var demo = new Vue({
+    el: '#app',
+    data: {
+        'apptitle': 'Django Vue.JS Job Board',
+        'jobs': [],
+        title: 'vue blog',
+        blog_list:[
+            {
+                'title': '처음 타보는 퍼스트 클래스, ANA NH211 - 후편',
+                'category': 'travel',
+                'date': '03 Oct 2016',
+                'link': '/blog/detail/'
+            }
+        ]
+    },
+    methods: {
+        addJob: function () {
+            var newJob = {
+                jobtitle: this.jobtitle.trim(),
+                jobdescription: this.jobdescription.trim()
+            };
+
+            this.$http.post('http://127.0.0.1:8000/api/jobs/', newJob);
+        },
+        removeJob: function (index) {
+            this.$http.delete('http://127.0.0.1:8000/api/jobs/'.concat(this.jobs[index].id));
+            this.jobs.splice(index, 1);
+        }
+    },
+    ready: function()
+        {
+            console.log(this.title);
+            console.log("vue") ;
+        }
+});
---
+++ static/vuejs/blog-list.js
@@ -0,0 +1,18 @@
+
+
+
+
+var blogList = new Vue({
+    el: '#blog_list',
+    data: {
+
+    },
+    methods: {
+
+    },
+    ready: function()
+        {
+            console.log("blog list");
+            console.log(this.blog_list)
+        }
+});
